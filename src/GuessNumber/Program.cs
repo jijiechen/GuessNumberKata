990bd33ee228642d2f9a96657188c457ef92a4a2
Jijie Chen <jijie.chen@someplace.com>
2016-06-05 18:36:58 +0800

    Added xunit.runner.visualstudio to support running tests using Visual Studio Test Runner

--- src/GuessNumber/Program.cs
+++ src/GuessNumber/Program.cs
@@ -10,6 +10,15 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
+
+
+            var game = new Game("1234");
+
+            var result = game.Guess("1234");
+
+            Console.WriteLine(result);
+
+
         }
     }
 }
