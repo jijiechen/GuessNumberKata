e1b7d455f6fd500c4a74712447341fabda9e508b
Jijie Chen <jijie.chen@someplace.com>
2016-06-05 21:06:27 +0800

    Introduced new fake input output implemention class

--- src/GuessNumber.Tests/GameFacadeFacts.cs
+++ src/GuessNumber.Tests/GameFacadeFacts.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿using GuessNumber.Tests.TestDouble;
 using Xunit;
 
 namespace GuessNumber.Tests
@@ -8,50 +8,30 @@ namespace GuessNumber.Tests
         [Fact]
         public void should_show_welcome_text()
         {
-            var inputStream = new MemoryStream();
-            var outputStream  = new MemoryStream();
+            using (var io = new FakeInputOutput())
+            {
+                io.In("7634");
 
-            WriteLineTo(inputStream, "7634");
+                var facade = new GameFacade(io.InReader, io.OutWriter);
+                facade.StartGame(new Game("1234"));
 
-            var facade = new GameFacade(new StreamReader(inputStream), new StreamWriter(outputStream));
-            facade.StartGame(new Game("1234"));
-
-            var ouputContent = ReadAllFrom(outputStream);
-            Assert.Contains("please input your guess", ouputContent);
+                Assert.Contains("please input your guess", io.Out());
+            }
         }
 
 
         [Fact]
         public void should_guess_numbers()
         {
-            var inputStream = new MemoryStream();
-            var outputStream  = new MemoryStream();
+            using (var io = new FakeInputOutput())
+            {
+                io.In("7634");
 
-            WriteLineTo(inputStream, "7634");
+                var facade = new GameFacade(io.InReader, io.OutWriter);
+                facade.StartGame(new Game("1234"));
 
-
-            var facade = new GameFacade(new StreamReader(inputStream), new StreamWriter(outputStream));
-            facade.StartGame(new Game("1234"));
-
-            var ouputContent = ReadAllFrom(outputStream);
-            Assert.Contains("2A0B", ouputContent);
-        }
-
-        private static void WriteLineTo(MemoryStream stream, string content)
-        {
-            var lastPosition = stream.Position;
-            var inputWriter = new StreamWriter(stream);
-
-            inputWriter.Write(content);
-            inputWriter.Flush();
-            stream.Seek(lastPosition, SeekOrigin.Begin);
-        }
-
-        private static string ReadAllFrom(MemoryStream outputStream)
-        {
-            outputStream.Seek(0, SeekOrigin.Begin);
-            var outputReader = new StreamReader(outputStream);
-            return outputReader.ReadToEnd();
+                Assert.Contains("2A0B", io.Out());
+            }
         }
     }
 }
---
+++ src/GuessNumber.Tests/TestDouble/FakeInputOutput.cs
@@ -0,0 +1,77 @@
+using System;
+using System.IO;
+
+namespace GuessNumber.Tests.TestDouble
+{
+    public class FakeInputOutput : IDisposable
+    {
+        private readonly Stream _inputStream; 
+        private readonly Stream _outputStream;
+        private StreamReader _inReader;
+        private StreamWriter _outWriter;
+
+        private StreamWriter _inWriter;
+        private StreamReader _outReader;
+
+        public FakeInputOutput()
+        {
+            _inputStream = new MemoryStream();
+            _outputStream = new MemoryStream();
+        }
+
+
+        public void In(string content)
+        {
+            var lastPosition = _inputStream.Position;
+
+            InWriter.Write(content);
+            InWriter.Flush();
+
+            _inputStream.Seek(lastPosition, SeekOrigin.Begin);
+        }
+
+        public string Out()
+        {
+            _outputStream.Seek(0, SeekOrigin.Begin);
+            return OutReader.ReadToEnd();
+        }
+
+        public StreamReader InReader
+        {
+            get { return _inReader ?? (_inReader = new StreamReader(_inputStream)); }
+        }
+
+        public StreamWriter OutWriter
+        {
+            get { return _outWriter ?? (_outWriter = new StreamWriter(_outputStream)); }
+        }
+
+
+        private StreamWriter InWriter
+        {
+            get { return _inWriter ?? (_inWriter = new StreamWriter(_inputStream)); }
+        }
+
+        private StreamReader OutReader
+        {
+            get { return _outReader ?? (_outReader = new StreamReader(_outputStream)); }
+        }
+
+        void IDisposable.Dispose()
+        {
+            _inputStream.Dispose();
+            if (_inReader != null)
+                _inReader.Dispose();
+            if (_inWriter != null)
+                _inWriter.Dispose();
+
+
+            _outputStream.Dispose();
+            if (_outReader != null)
+                _outReader.Dispose();
+            if(_outWriter != null)
+                _outWriter.Dispose();
+            
+        }
+    }
+}
