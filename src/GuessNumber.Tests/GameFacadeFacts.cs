844f634f9facb9d63aa50bba8db6d610f252b7ab
Jijie Chen <jijie.chen@someplace.com>
2016-06-05 20:49:35 +0800

    Extracted write to and read from stream methods

--- src/GuessNumber.Tests/GameFacadeFacts.cs
+++ src/GuessNumber.Tests/GameFacadeFacts.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Xunit;
 
 namespace GuessNumber.Tests
@@ -11,49 +10,48 @@ namespace GuessNumber.Tests
         {
             var inputStream = new MemoryStream();
             var outputStream  = new MemoryStream();
-            var userInput = new StreamReader(inputStream);
-            var gameOutput = new StreamWriter(outputStream);
 
-            var inputWriter = new StreamWriter(inputStream);
-            var lastPosition = inputStream.Position;
-            inputWriter.Write("7634");
-            inputWriter.Flush();
-            inputStream.Seek(lastPosition, SeekOrigin.Begin);
+            WriteLineTo(inputStream, "7634");
 
-            var facade = new GameFacade(userInput, gameOutput);
+            var facade = new GameFacade(new StreamReader(inputStream), new StreamWriter(outputStream));
             facade.StartGame(new Game("1234"));
 
-            outputStream.Seek(0, SeekOrigin.Begin);
-            var outputReader = new StreamReader(outputStream);
-            var ouputContent = outputReader.ReadToEnd();
+            var ouputContent = ReadAllFrom(outputStream);
             Assert.Contains("please input your guess", ouputContent);
         }
-        
-        
-        
+
+
         [Fact]
         public void should_guess_numbers()
         {
             var inputStream = new MemoryStream();
             var outputStream  = new MemoryStream();
-            var userInput = new StreamReader(inputStream);
-            var gameOutput = new StreamWriter(outputStream);
 
-            var inputWriter = new StreamWriter(inputStream);
-            var lastPosition = inputStream.Position;
-            inputWriter.Write("7634");
-            inputWriter.Flush();
-            inputStream.Seek(lastPosition, SeekOrigin.Begin);
+            WriteLineTo(inputStream, "7634");
 
 
-            var facade = new GameFacade(userInput, gameOutput);
+            var facade = new GameFacade(new StreamReader(inputStream), new StreamWriter(outputStream));
             facade.StartGame(new Game("1234"));
 
-            outputStream.Seek(0, SeekOrigin.Begin);
-            var outputReader = new StreamReader(outputStream);
-            var ouputContent = outputReader.ReadToEnd();
+            var ouputContent = ReadAllFrom(outputStream);
             Assert.Contains("2A0B", ouputContent);
         }
 
+        private static void WriteLineTo(MemoryStream stream, string content)
+        {
+            var lastPosition = stream.Position;
+            var inputWriter = new StreamWriter(stream);
+
+            inputWriter.Write(content);
+            inputWriter.Flush();
+            stream.Seek(lastPosition, SeekOrigin.Begin);
+        }
+
+        private static string ReadAllFrom(MemoryStream outputStream)
+        {
+            outputStream.Seek(0, SeekOrigin.Begin);
+            var outputReader = new StreamReader(outputStream);
+            return outputReader.ReadToEnd();
+        }
     }
 }
