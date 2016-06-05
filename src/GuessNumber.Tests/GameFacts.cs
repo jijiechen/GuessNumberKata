217f1433568742576c64cbd7ff79d7ab76845fd9
Jijie Chen <jijie.chen@someplace.com>
2016-06-05 15:59:17 +0800

    Passed first test: 0A0B

---
+++ src/GuessNumber.Tests/GameFacts.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+
+namespace GuessNumber.Tests
+{
+    public class GameFacts
+    {
+
+        [Fact]
+        public void should_output_0A0B_when_no_number_matches()
+        {
+            var random = "1234";
+            var guess = "5678";
+
+            var game = new Game(random);
+
+
+            var result = game.Guess(guess);
+            Assert.Equal("0A0B", result);
+        }
+
+
+
+    }
+}
---
+++ src/GuessNumber/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    public class Game
+    {
+        public Game(string random)
+        {
+            
+        }
+
+        public string Guess(string guess)
+        {
+            return "0A0B";
+        }
+    }
+}
